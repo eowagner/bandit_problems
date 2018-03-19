8ab1e670494119c8d6be52157656b17f34dca4b4
eowagner <e.o.wagner@gmail.com>
2018-03-19 14:20:02 -0500

    r&r simulations with alternatives structures

--- supplemental_simulations.sh
+++ supplemental_simulations.sh
@@ -11,3 +11,7 @@ mkdir -p $DNAME
 $PROG bernoulli_simulations/baseline_agents_parent.js -p uniform -r $RUNS -s 1000 -q .6 | tee "$DNAME/baseline-agents-6.csv"
 $PROG bernoulli_simulations/special_conduct_agents_parent.js -p uniform -r $RUNS -s 1000 -n 1 -c randomize -q .6 | tee "$DNAME/special-conduct-agents-6.csv"
 $PROG bernoulli_simulations/conduct_agents_parent.js -p uniform -r $RUNS -s 1000 -n 2 -c randomize -q .6 | tee "$DNAME/conduct-agents-6.csv"
+$PROG bernoulli_simulations/baseline_p_parent.js -p uniform -r $RUNS -s 1000 -n 9 | tee "$DNAME/baseline-p.csv"
+$PROG bernoulli_simulations/diss_p_parent.js -p uniform -r $RUNS -s 1000 -n 9 -c randomize | tee "$DNAME/star-p.csv"
+$PROG bernoulli_simulations/alt_structs_diss_p_parent.js -p uniform -r $RUNS -s 1000 -n 9 -c randomize -a cycle | tee "$DNAME/cycle-p.csv"
+$PROG bernoulli_simulations/alt_structs_diss_p_parent.js -p uniform -r $RUNS -s 1000 -n 9 -c randomize -a wheel | tee "$DNAME/wheel-p.csv"
