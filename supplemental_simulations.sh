eeae4d6c4b9c49ec32b2fe622350b3214ebf83f2
eowagner <e.o.wagner@gmail.com>
2018-03-26 14:06:25 -0500

    100k runs for alternative graph structures as the number of agents is varied

--- supplemental_simulations.sh
+++ supplemental_simulations.sh
@@ -3,14 +3,13 @@
 TIMESTAMP="$(date +%Y%m%d%H%M%S)"
 DNAME="out/$TIMESTAMP"
 # RUNS=100000
-RUNS=1000
+RUNS=100000
 PROG="nodejs"
 
 mkdir -p $DNAME
 
 # $PROG bernoulli_simulations/special_conduct_p_parent.js -p uniform -r $RUNS -s 1000 -n 9 | tee "$DNAME/special-conduct-p.csv"
 # $PROG bernoulli_simulations/special_conduct_agents_parent.js -p uniform -r $RUNS -s 1000 -q .6 | tee "$DNAME/special-conduct-agents-6.csv"
-# $PROG bernoulli_simulations/diss_p_parent.js -p uniform -r $RUNS -s 1000 -n 9 -c randomize | tee "$DNAME/star-p.csv"
 # $PROG bernoulli_simulations/alt_structs_diss_p_parent.js -p uniform -r $RUNS -s 1000 -n 9 -c randomize -a kcycle -k 1 | tee "$DNAME/cycle-1-p.csv"
 # $PROG bernoulli_simulations/alt_structs_diss_p_parent.js -p uniform -r $RUNS -s 1000 -n 9 -c randomize -a kcycle -k 2 | tee "$DNAME/cycle-2-p.csv"
 # $PROG bernoulli_simulations/alt_structs_diss_p_parent.js -p uniform -r $RUNS -s 1000 -n 9 -c randomize -a kcycle -k 3 | tee "$DNAME/cycle-3-p.csv"
